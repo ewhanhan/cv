eb1f7905edac15b2d66e4f8a2c0dde2d44988d06
Ewhan <ewhan.han@gmail.com>
2023-12-27 00:29:11 -0500

    Add Education component

---
+++ src/components/education.tsx
@@ -0,0 +1,32 @@
+import { Card, CardContent, CardHeader } from "@/components/ui/card";
+import { type ResumeData } from "@/data/resume-data";
+import React from "react";
+
+interface EducationProps {
+  education: ResumeData["education"];
+}
+
+export const Education: React.FC<EducationProps> = ({ education }) => {
+  return (
+    <>
+      <h2 className="text-xl font-bold">Education</h2>
+      {education.map((education) => {
+        return (
+          <Card key={education.school}>
+            <CardHeader>
+              <div className="flex items-center justify-between gap-x-2 text-base">
+                <h3 className="font-semibold leading-none">
+                  {education.school}
+                </h3>
+                <div className="text-sm tabular-nums text-gray-500">
+                  {education.start} - {education.end}
+                </div>
+              </div>
+            </CardHeader>
+            <CardContent className="mt-2">{education.degree}</CardContent>
+          </Card>
+        );
+      })}
+    </>
+  );
+};
